7b5d13836cdb05695b8f09515f51a96710063309
Chaitanya-Chaurasia <98615500+Chaitanya-Chaurasia@users.noreply.github.com>
2023-08-07 19:55:27 +0530

    Created app structure with sample navbar, first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import Navbar from "./Components/Navbar";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <div className="container">Forex-Guru</div>
+    </>
+  );
+}
+
+export default App;
